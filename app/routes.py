91c46afb0742b2d0daf9c3e283912920224cfca5
Megan Gonzalez <meganmgonzalez18@icloud.com>
2023-04-24 11:52:38 -0400

    Get one book request. Handled 404 and 400 errors.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
+
 
 class Crystal:
     def __init__(self, id, name, color, power):
@@ -13,18 +14,49 @@ crystals = [
     Crystal(3, 'Rose Quartz', 'Pink', 'Love')
 ]
 
-crystal_bp = Blueprint('crystals', __name__, url_prefix = '/crystals')
+def validate_crystal(crystal_id):
+    try:
+        crystal_id == int(crystal_id)
+    except:
+        abort(make_response({"message": f"{crystal_id} is not a valid type {type(crystal_id)}. Must be an integer."}, 400))
 
-@crystal_bp.route('', methods = ['GET'])
+    for crystal in crystals:
+        if crystal.id == crystal_id:
+            return crystal
+    
+    abort(make_response({"message": f"crystal {crystal_id} not found."}, 404))
+
+
+
+crystal_bp = Blueprint("crystals", __name__, url_prefix = "/crystals")
+
+@crystal_bp.route("", methods = ["GET"])
 
 def handle_crystals():
     crystal_response = []
     for crystal in crystals:
         crystal_response.append({
-            'id': crystal.id,
-            'name': crystal.name,
-            'color': crystal.color,
-            'power': crystal.power
+            "id": crystal.id,
+            "name": crystal.name,
+            "color": crystal.color,
+            "power": crystal.power
         })
 
     return jsonify(crystal_response)
+
+@crystal_bp.route("/<crystal_id>", methods = ["GET"])
+
+def handle_crystal(crystal_id):
+    
+    crystal = validate_crystal(crystal_id)
+
+    return {
+        "id": crystal.id,
+        "name": crystal.name,
+        "color": crystal.color,
+        "power": crystal.power
+    }
+
+
+
+
